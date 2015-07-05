9bc33ae8fd8ab3ed7900d237bb7208661731db8a
Shawn Krisman <shawn@everlane.com>
2015-07-04 19:46:03 -0700

    we move the block down now.

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-var BLOCK_WIDTH = 60;
-var BLOCK_HEIGHT = 80;
+var BLOCK_WIDTH = 50;
+var BLOCK_HEIGHT = 50;
 var BOARD_BLOCK_WIDTH = 6;
 var BOARD_BLOCK_HEIGHT = 12;
 var BOARD_WIDTH = BLOCK_WIDTH * BOARD_BLOCK_WIDTH;
@@ -16,7 +16,28 @@ Crafty.c('Block', {
 
     create: function(x, y) {
         this.attr({x: x * BLOCK_WIDTH, y: y * BLOCK_HEIGHT});
+    },
+
+    moveDown: function() {
+        this.y += BLOCK_HEIGHT;
     }
 });
 
-Crafty.e('Block').attr({x: 0, y: 0}).color('#F00');
+Crafty.c('Board', {
+    init: function() {
+        this.requires('2D, DOM, Color, Delay');
+        this.attr({w: BOARD_WIDTH, h: BOARD_HEIGHT, x: 0, y: 0});
+        this.color('green');
+        this.block = Crafty.e('Block').attr({x: 0, y: 0}).color('#F00');
+        this.attach(this.block);
+        this.delay(this.updateState, 1000, 10);
+    },
+
+    updateState: function() {
+        this.block.moveDown();
+    }
+});
+
+
+
+var board = Crafty.e('Board');
