ab2f887f944417af700e0a47e600f5a72f133cdc
Shawn Krisman <shawn@everlane.com>
2015-07-21 12:45:18 -0500

    we have a seperate blocks class now.

--- game.js
+++ game.js
@@ -28,15 +28,12 @@ Crafty.c('Block', {
     }
 });
 
-Crafty.c('Board', {
+Crafty.c('Blocks', {
     init: function() {
-        this.requires('2D, DOM, Color, Delay');
+        this.requires('2D, DOM');
         this.attr({w: BOARD_WIDTH, h: BOARD_HEIGHT, x: 0, y: 0});
-        this.color('green');
         this.appendBlock(0, 0);
         this.blocks = [];
-        this.delay(this.updateState, 1000, -1);
-        this.bind('KeyUp', this.handleKeyPress);
     },
 
     appendBlock: function(x, y) {
@@ -44,7 +41,13 @@ Crafty.c('Board', {
         this.attach(this.block);
     },
 
-    updateState: function() {
+    shiftBlock: function(delta) {
+        var position = this.block.getPosition();
+        this.block.movePosition({x: delta.x, y: delta.y});
+    },
+
+
+    tick: function() {
         this.shiftBlock({x: 0, y: 1});
         var pos = this.block.getPosition();
         if (pos.y === BOARD_BLOCK_HEIGHT - 1) {
@@ -53,19 +56,44 @@ Crafty.c('Board', {
         }
     },
 
-    shiftBlock: function(delta) {
-        var position = this.block.getPosition();
-        this.block.movePosition({x: delta.x, y: delta.y});
+    moveLeft: function() {
+        var pos = this.block.getPosition();
+        if(pos.x !== 0) {
+            this.block.movePosition({x: -1, y: 0});
+        }
+    },
+
+    moveRight: function() {
+        var pos = this.block.getPosition();
+        if(pos.x !== BOARD_BLOCK_WIDTH - 1) {
+            this.block.movePosition({x: 1, y: 0});
+        }
+    },
+});
+
+Crafty.c('Board', {
+    init: function() {
+        this.requires('2D, DOM, Color, Delay');
+        this.attr({w: BOARD_WIDTH, h: BOARD_HEIGHT, x: 0, y: 0});
+        this.color('green');
+        this.blocks = Crafty.e('Blocks');
+        this.attach(this.blocks);
+        this.delay(this.blocks.tick.bind(this.blocks), 1000, -1);
+        this.bind('KeyUp', this.handleKeyPress);
     },
 
     handleKeyPress: function(e) {
-        var pos = this.block.getPosition();
-        if(e.key == Crafty.keys.LEFT_ARROW && pos.x !== 0) {
-            this.block.movePosition({x: -1, y: 0});
-        } else if (e.key == Crafty.keys.RIGHT_ARROW && pos.x !== BOARD_BLOCK_WIDTH - 1) {
-            this.block.movePosition({x: 1, y: 0});
+        if(e.key == Crafty.keys.LEFT_ARROW) {
+            this.blocks.moveLeft();
+        } else if (e.key == Crafty.keys.RIGHT_ARROW) {
+            this.blocks.moveRight();
         }
     }
 });
+
+
+
+
+
 
 var board = Crafty.e('Board');
