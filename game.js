1a08043d17af581df72b6de52e2f5898499f4ba6
Shawn Krisman <krisman.shawn@gmail.com>
2016-08-19 22:46:56 -0700

    using the motion components now.

--- game.js
+++ game.js
@@ -1,137 +1,46 @@
 "use strict";
 
-var BLOCK_WIDTH = 50;
-var BLOCK_HEIGHT = 50;
-var BOARD_BLOCK_WIDTH = 6;
-var BOARD_BLOCK_HEIGHT = 12;
+var BLOCK_WIDTH = 25;
+var BLOCK_HEIGHT = 25;
+var BOARD_BLOCK_WIDTH = 20;
+var BOARD_BLOCK_HEIGHT = 10;
 var BOARD_WIDTH = BLOCK_WIDTH * BOARD_BLOCK_WIDTH;
 var BOARD_HEIGHT = BLOCK_HEIGHT * BOARD_BLOCK_HEIGHT;
-var BLOCK_SPEED = 2 // pixels per second
+var BLOCK_SPEED = 40 // pixels per second
 
 
 Crafty.init(BOARD_WIDTH, BOARD_HEIGHT, document.getElementById('game'));
-Crafty.sprite("tiles.png", {RedTile:[0, 0, 50, 50]});
+Crafty.sprite("tiles.png", {RedTile: [3 * BLOCK_WIDTH, 0, BLOCK_WIDTH, BLOCK_HEIGHT]});
 
 Crafty.c('Block', {
-    init: function() {
-        this.requires('2D, DOM, RedTile');
-        this.attr({w: BLOCK_WIDTH, h: BLOCK_HEIGHT})
-    },
+  init: function() {
+    this.requires('2D, Canvas, Motion, RedTile');
+    this.attr({w: BLOCK_WIDTH, h: BLOCK_HEIGHT, vy: BLOCK_SPEED})
+  },
 
-    create: function(x, y) {
-        this.attr({x: x * BLOCK_WIDTH, y: y * BLOCK_HEIGHT});
-        return this;
-    },
-
-    movePosition: function(delta) {
-        this.x += delta.x * BLOCK_WIDTH;
-        this.y += delta.y * BLOCK_HEIGHT;
-    },
-
-    getPosition: function() {
-        return {x: this.x / BLOCK_WIDTH, y: this.y / BLOCK_HEIGHT};
-    },
-
-    willCollideWith: function(block, dx, dy) {
-        return this.x + dx < block.x + BLOCK_WIDTH &&
-            this.x + dx + BLOCK_WIDTH > block.x &&
-            this.y + dy < block.y + BLOCK_HEIGHT &&
-            this.y + dy + BLOCK_HEIGHT > block.y;
-    }
-});
-
-Crafty.c('Blocks', {
-    init: function() {
-        this.requires('2D, DOM');
-        this.attr({w: BOARD_WIDTH, h: BOARD_HEIGHT, x: 0, y: 0});
-        this.createNewBlock();
-        this.blocks = [];
-    },
-
-    createNewBlock: function() {
-        this.appendBlock(3, 0);
-    },
-
-    appendBlock: function(x, y) {
-        this.block = Crafty.e('Block').create(x, y);
-        this.attach(this.block);
-    },
-
-    shiftBlock: function(delta) {
-        if (this.block === null) return;
-        var position = this.block.getPosition();
-        this.block.movePosition({x: delta.x, y: delta.y});
-    },
-
-    tick: function(frameData) {
-        if (this.block === null) {
-            this.createNewBlock();
-            return;
-        }
-
-        this.shiftBlock({x: 0, y: frameData.dt / 1000 * BLOCK_SPEED});
-        var pos = this.block.getPosition();
-        if (pos.y >= BOARD_BLOCK_HEIGHT - 1 || this.collidesWith(0, 1)) {
-            this.blocks.push(this.block);
-            this.block = null;
-        }
-    },
-
-    moveLeft: function() {
-        if (this.block === null) return;
-        var pos = this.block.getPosition();
-        if(pos.x !== 0 && !this.collidesWith(-1, 0)) {
-            this.block.movePosition({x: -1, y: 0});
-        }
-    },
-
-    moveRight: function() {
-        if (this.block === null) return;
-        var pos = this.block.getPosition();
-        if(pos.x !== BOARD_BLOCK_WIDTH - 1 && !this.collidesWith(1, 0)) {
-            this.block.movePosition({x: 1, y: 0});
-        }
-    },
-
-    collidesWith: function(dx, dy) {
-        for (let block of this.blocks) {
-            if (this.block.willCollideWith(block, dx, dy)) {
-                return true
-            }
-        }
-        return false;
-    },
-
-    hasBlockAt: function(x, y) {
-        var hasBlock = false
-        this.blocks.forEach(function(block) {
-            var pos = block.getPosition()
-            if (pos.x === x && pos.y === y) {
-                hasBlock = true;
-            }
-        });
-        return hasBlock;
-    },
+  create: function(x, y) {
+    this.attr({x: x * BLOCK_WIDTH, y: y * BLOCK_HEIGHT});
+    return this;
+  },
 });
 
 Crafty.c('Board', {
-    init: function() {
-        this.requires('2D, DOM, Color, Delay');
-        this.attr({w: BOARD_WIDTH, h: BOARD_HEIGHT, x: 0, y: 0});
-        this.color('green');
-        this.blocks = Crafty.e('Blocks');
-        this.attach(this.blocks);
-        this.bind('KeyDown', this.handleKeyPress);
-        this.bind('EnterFrame', this.blocks.tick.bind(this.blocks));
-    },
+  init: function() {
+    this.requires('2D, Canvas, Color');
+    this.attr({w: BOARD_WIDTH, h: BOARD_HEIGHT, x: 0, y: 0});
+    this.color('green');
+    this.block = Crafty.e('Block').create(3, 0);
+    this.attach(this.block);
+    this.bind('KeyDown', this.handleKeyPress);
+  },
 
-    handleKeyPress: function(e) {
-        if(e.key == Crafty.keys.LEFT_ARROW) {
-            this.blocks.moveLeft();
-        } else if (e.key == Crafty.keys.RIGHT_ARROW) {
-            this.blocks.moveRight();
-        }
+  handleKeyPress: function(e) {
+    if(e.key == Crafty.keys.LEFT_ARROW) {
+      this.blocks.moveLeft();
+    } else if (e.key == Crafty.keys.RIGHT_ARROW) {
+      this.blocks.moveRight();
     }
+  }
 });
 
 
