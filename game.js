84b58a258d4f211ba9e171be9b9a4b70dbcb0660
Shawn Krisman <krisman.shawn@gmail.com>
2016-08-27 18:35:38 -0700

    we control a player now.

--- game.js
+++ game.js
@@ -12,11 +12,10 @@ var BLOCK_SPEED = BLOCK_HEIGHT // pixels per second
 Crafty.init(BOARD_WIDTH, BOARD_HEIGHT, document.getElementById('game'));
 Crafty.sprite("tiles.png", {Tile: [5 * BLOCK_WIDTH, 0, BLOCK_WIDTH, BLOCK_HEIGHT]});
 
-Crafty.c('Block', {
+Crafty.c('Player', {
   init: function() {
-    this.requires('2D, Canvas, Motion, Collision, Tile');
+    this.requires('2D, Canvas, Tile');
     this.attr({w: BLOCK_WIDTH, h: BLOCK_HEIGHT, vy: BLOCK_SPEED})
-    this.checkHits('Floor, Block')
     this.bind('HitOn', this._onHit.bind(this));
   },
 
@@ -31,9 +30,21 @@ Crafty.c('Block', {
     }
   },
 
-  moveRight: function() {
+  moveRight: function(x, y) {
     if (this.x !== BLOCK_WIDTH * (BOARD_BLOCK_WIDTH - 1)) {
       this.x += BLOCK_WIDTH;
+    }
+  },
+
+  moveUp: function() {
+    if (this.y !== 0) {
+      this.y -= BLOCK_HEIGHT;
+    }
+  },
+
+  moveDown: function() {
+    if (this.y !== BLOCK_HEIGHT * (BOARD_BLOCK_HEIGHT - 1)) {
+      this.y += BLOCK_HEIGHT;
     }
   },
 
@@ -45,36 +56,31 @@ Crafty.c('Block', {
 
 });
 
-Crafty.c('Floor', {
-  init: function() {
-    this.requires('Collision');
-    this.attr({w: BOARD_WIDTH, h: 1, x: 0, y: BOARD_HEIGHT});
-  }
-});
-
 Crafty.c('Board', {
   init: function() {
     this.requires('2D, Canvas, Color, Keyboard');
     this.attr({w: BOARD_WIDTH, h: BOARD_HEIGHT, x: 0, y: 0});
     this.color('green');
     this.bind('KeyDown', this._onKeyDown.bind(this));
-    this._spawnNewBlock();
+    this._spawnPlayer();
   },
 
-  _spawnNewBlock: function() {
-    this.block = Crafty.e('Block').create(3, 0);
-    this.block.bind('Collision', this._spawnNewBlock.bind(this));
-    this.attach(this.block);
+  _spawnPlayer: function() {
+    this.player = Crafty.e('Player').create(9, 6);
+    this.attach(this.player);
   },
 
   _onKeyDown: function(e) {
     if (e.key == Crafty.keys.LEFT_ARROW) {
-      this.block.moveLeft();
+      this.player.moveLeft();
     } else if (e.key == Crafty.keys.RIGHT_ARROW) {
-      this.block.moveRight();
+      this.player.moveRight();
+    } else if (e.key == Crafty.keys.UP_ARROW) {
+      this.player.moveUp();
+    } else if (e.key == Crafty.keys.DOWN_ARROW) {
+      this.player.moveDown();
     }
   }
 });
 
 var board = Crafty.e('Board');
-var floor = Crafty.e('Floor');
