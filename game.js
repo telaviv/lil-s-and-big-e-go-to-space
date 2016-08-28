c76c844b692a8a09a8db18473897214fd83ab3c7
Shawn Krisman <krisman.shawn@gmail.com>
2016-08-27 23:26:03 -0700

    all the level stuff is there.

--- game.js
+++ game.js
@@ -2,50 +2,78 @@
 
 var BLOCK_WIDTH = 25;
 var BLOCK_HEIGHT = BLOCK_WIDTH;
-var BOARD_BLOCK_WIDTH = 20;
-var BOARD_BLOCK_HEIGHT = 10;
+var BOARD_BLOCK_WIDTH = 25;
+var BOARD_BLOCK_HEIGHT = 15;
 var BOARD_WIDTH = BLOCK_WIDTH * BOARD_BLOCK_WIDTH;
 var BOARD_HEIGHT = BLOCK_HEIGHT * BOARD_BLOCK_HEIGHT;
 var BLOCK_SPEED = BLOCK_HEIGHT // pixels per second
 
 
 Crafty.init(BOARD_WIDTH, BOARD_HEIGHT, document.getElementById('game'));
-Crafty.sprite("tiles.png", {Tile: [5 * BLOCK_WIDTH, 0, BLOCK_WIDTH, BLOCK_HEIGHT]});
+Crafty.sprite("tiles.png", {
+    PlayerSprite: [5 * BLOCK_WIDTH, 0, BLOCK_WIDTH, BLOCK_HEIGHT],
+    PlantSprite: [0, 0, BLOCK_WIDTH, BLOCK_HEIGHT],
+});
 
-Crafty.c('Player', {
+Crafty.c('KeyboardMovement', {
   init: function() {
-    this.requires('2D, Canvas, Tile');
+    this.requires('Keyboard');
+    this.bind('KeyDown', this._onKeyDown.bind(this));
+  },
+
+  _onKeyDown: function(e) {
+    if (e.key == Crafty.keys.LEFT_ARROW) {
+      this.moveLeft();
+    } else if (e.key == Crafty.keys.RIGHT_ARROW) {
+      this.moveRight();
+    } else if (e.key == Crafty.keys.UP_ARROW) {
+      this.moveUp();
+    } else if (e.key == Crafty.keys.DOWN_ARROW) {
+      this.moveDown();
+    }
+  },
+
+  moveLeft: function() {
+    if (this.x !== this.bb.x) {
+      this.x -= this.w;
+    }
+  },
+
+  moveRight: function(x, y) {
+    if (this.x !== this.bb.w + this.bb.x - this.w) {
+      this.x += this.w;
+    }
+  },
+
+  moveUp: function() {
+    if (this.y !== this.bb.y) {
+      this.y -= this.h;
+    }
+  },
+
+  moveDown: function() {
+    if (this.y !== this.bb.h + this.bb.y - this.h) {
+      this.y += this.h;
+    }
+  },
+});
+
+Crafty.c('Tile', {
+  init: function() {
+    this.requires('2D, Canvas');
     this.attr({w: BLOCK_WIDTH, h: BLOCK_HEIGHT, vy: BLOCK_SPEED})
-    this.bind('HitOn', this._onHit.bind(this));
   },
 
   create: function(x, y) {
     this.attr({x: x * BLOCK_WIDTH, y: y * BLOCK_HEIGHT});
     return this;
   },
+});
 
-  moveLeft: function() {
-    if (this.x !== 0) {
-      this.x -= BLOCK_WIDTH;
-    }
-  },
-
-  moveRight: function(x, y) {
-    if (this.x !== BLOCK_WIDTH * (BOARD_BLOCK_WIDTH - 1)) {
-      this.x += BLOCK_WIDTH;
-    }
-  },
-
-  moveUp: function() {
-    if (this.y !== 0) {
-      this.y -= BLOCK_HEIGHT;
-    }
-  },
-
-  moveDown: function() {
-    if (this.y !== BLOCK_HEIGHT * (BOARD_BLOCK_HEIGHT - 1)) {
-      this.y += BLOCK_HEIGHT;
-    }
+Crafty.c('Player', {
+  init: function() {
+    this.requires('Tile, KeyboardMovement, PlayerSprite');
+    this.z = 1;
   },
 
   _onHit: function() {
@@ -56,31 +84,53 @@ Crafty.c('Player', {
 
 });
 
+Crafty.c('Plant', {
+  init: function() {
+    this.requires('Tile, PlantSprite');
+  },
+});
+
+Crafty.c('Water', {
+  init: function() {
+    this.requires('Tile, Color');
+    this.color('blue');
+  },
+})
+
+
 Crafty.c('Board', {
   init: function() {
     this.requires('2D, Canvas, Color, Keyboard');
-    this.attr({w: BOARD_WIDTH, h: BOARD_HEIGHT, x: 0, y: 0});
+    this.attr({
+      w: BLOCK_WIDTH * 20 ,
+      h: BLOCK_HEIGHT * 10,
+      x: BLOCK_WIDTH,
+      y: BLOCK_HEIGHT
+    });
     this.color('green');
-    this.bind('KeyDown', this._onKeyDown.bind(this));
     this._spawnPlayer();
+    this._spawnPlant();
+    this._spawnWater();
   },
 
   _spawnPlayer: function() {
     this.player = Crafty.e('Player').create(9, 6);
+    this.player.shift(this.x, this.y);
+    this.player.bb = this;
     this.attach(this.player);
   },
 
-  _onKeyDown: function(e) {
-    if (e.key == Crafty.keys.LEFT_ARROW) {
-      this.player.moveLeft();
-    } else if (e.key == Crafty.keys.RIGHT_ARROW) {
-      this.player.moveRight();
-    } else if (e.key == Crafty.keys.UP_ARROW) {
-      this.player.moveUp();
-    } else if (e.key == Crafty.keys.DOWN_ARROW) {
-      this.player.moveDown();
-    }
-  }
+  _spawnPlant: function() {
+    var plant = Crafty.e('Plant').create(8, 6);
+    plant.shift(this.x, this.y);
+    this.attach(plant);
+  },
+
+  _spawnWater: function() {
+    var water = Crafty.e('Water').create(0, 0);
+    water.shift(this.x, this.y);
+  },
+
 });
 
 var board = Crafty.e('Board');
